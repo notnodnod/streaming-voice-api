1d431582c7bc87f8ba674e66c7acc89d761ddd4f
notnodnod <190180894+notnodnod@users.noreply.github.com>
2024-12-07 23:48:15 +0100

    First RealtimeTTS test (not working yet)

--- streaming_voice_api/foo.py
+++ streaming_voice_api/foo.py
@@ -13,5 +13,23 @@ def foo(bar: str) -> str:
     return bar
 
 
-if __name__ == "__main__":  # pragma: no cover
-    pass
+if __name__ == "__main__":
+    import logging
+
+    from RealtimeTTS import CoquiEngine, TextToAudioStream
+
+    def dummy_generator():
+        yield "Hey guys! These here are realtime spoken sentences based on local text synthesis. "
+        yield "With a local, neuronal, cloned voice. So every spoken sentence sounds unique."
+
+    level = logging.DEBUG
+    debug = True
+
+    print("Starting the test script")
+
+    source_voice = "voices/female_01.wav"
+    coqui_engine = CoquiEngine(voice=source_voice, level=level)
+    stream = TextToAudioStream(coqui_engine, level=level)
+    stream.feed(dummy_generator())
+    stream.play(output_wavfile="outputs/" + stream.engine.engine_name + "_output.wav", debug=debug)
+    coqui_engine.shutdown()
